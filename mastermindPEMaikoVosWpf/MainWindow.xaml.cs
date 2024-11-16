2ad703862744c52199bed38b2e34fa290ea3eb61
MaikoVosPXL <12401562@student.pxl.be>
2024-11-16 15:40:07 +0100

    MasterMind-02

--- mastermindPEMaikoVosWpf/MainWindow.xaml.cs
+++ mastermindPEMaikoVosWpf/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace mastermindPEMaikoVosWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        string randomColorOne, randomColorTwo, randomColorThree, randomColorFour;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindowLoader(object sender, RoutedEventArgs e)
+        {
+            randomColorOne = PickingRandomColor(rnd.Next(0, 6));
+            randomColorTwo = PickingRandomColor(rnd.Next(0, 6));
+            randomColorThree = PickingRandomColor(rnd.Next(0, 6));
+            randomColorFour = PickingRandomColor(rnd.Next(0, 6));
+            this.Title = $"MasterMind: {randomColorOne}, {randomColorTwo}, {randomColorThree}, {randomColorFour}";
+        }
+
+        private string PickingRandomColor(int randomNumber)
+        {
+            switch (randomNumber)
+            {
+                case 0:
+                    return "Red";
+                case 1:
+                    return "Yellow";
+                case 2:
+                    return "Orange";
+                case 3:
+                    return "White";
+                case 4:
+                    return "Green";
+                case 5:
+                    return "Blue";
+                default:
+                    return "Black";
+            }
         }
     }
 }
